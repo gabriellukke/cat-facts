de25410e923062b6b8ffae517614d3a62445dc53
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-07 15:57:11 -0300

    feat: disable get facts button in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+
 export default function Home() {
+  const [factsQuantity, setFactsQuantity] = useState(0);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setFactsQuantity(Number(value));
+  };
+
+  const hasFacts = factsQuantity > 0;
+
   return (
     <main>
       <h1 className="text-5xl">Cat Facts</h1>
       <form>
         <label htmlFor="facts-number">
           How many facts do you want to know?
-          <input type="number" id="facts-number" />
+          <input
+            type="number"
+            id="facts-number"
+            value={factsQuantity}
+            onChange={handleChange}
+          />
         </label>
-        <button type="button">Get facts</button>
+        <button type="button" disabled={!hasFacts}>
+          Get facts
+        </button>
       </form>
     </main>
   );
