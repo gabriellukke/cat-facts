e915bf4fc57e36faac8789a7907a46aea4347e4c
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-07 15:33:41 -0300

    tests: home page

---
+++ src/pages/Home/index.test.tsx
@@ -0,0 +1,50 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Home from '.';
+
+describe('Home page', () => {
+  it('should render the home page', async () => {
+    render(<Home />);
+
+    const heading = screen.getByRole('heading', {
+      name: /cat facts/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('can\t click on the button if there is no number of facts', () => {
+    render(<Home />);
+
+    const factsButton = screen.getByRole('button', {
+      name: /get facts/i,
+    });
+    expect(factsButton).toBeDisabled();
+
+    const factsInput = screen.getByRole('spinbutton');
+    userEvent.type(factsInput, '5');
+    expect(factsButton).toBeEnabled();
+
+    userEvent.clear(factsInput);
+    expect(factsButton).toBeDisabled();
+
+    userEvent.type(factsInput, '0');
+    expect(factsButton).toBeDisabled();
+  });
+
+  it('can choose number of facts', () => {
+    render(<Home />);
+
+    const factsInput = screen.getByRole('spinbutton');
+    userEvent.type(factsInput, '5');
+    expect(factsInput).toHaveValue(5);
+
+    const factsButton = screen.getByRole('button', {
+      name: /get facts/i,
+    });
+    userEvent.click(factsButton);
+
+    expect(factsInput).not.toBeInTheDocument();
+  });
+});
